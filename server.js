5264d8088f8d974b87375f5d070b749eede0705b
spencer barriball <sbarriball@me.com>
2022-04-21 14:22:24 +0100

    route order is important

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('im in the middleware');
-  next(); // allows it to goto the next middleware inline
+// order is important for matching!
+app.use('/add-product', (req, res, next) => {
+  console.log('im in the add product');
+  res.send('<h1>Add Product</h1>');
 });
 
-app.use((req, res, next) => {
-  console.log('im in another middleware');
+app.use('/', (req, res, next) => {
+  console.log('im in the middleware');
   res.send('<h1>Hello from Express!</h1>');
 });
 
